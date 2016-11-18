913dedb27f794d5174ebfbfef7e1790b9510caaf
Corwin <corstreet@gmail.com>
2016-11-18 00:03:39 -0800

    First commit for portfolio build v1

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	smoothScroll.init({
+		speed: 1500,
+		easing: 'easeInOutCubic',
+		offset: 86
+	});
+
+	fixedNav();
+
+	$(window).scroll(function(){
+		fixedNav();	
+	});
+	function fixedNav () {
+		var scroll = $(window).scrollTop();
+		var navHeight = $('.navbar').outerHeight();
+		if (scroll >= 25){
+			$('.navbar').addClass('fixed');
+
+		} else {
+			$('.navbar').removeClass('fixed');		
+		}
+		if ( ( scroll + navHeight ) >= ($('#about').offset().top) ){
+			$('a[href="#about"]').addClass('active');
+		} else {
+			$('a[href="#about"]').removeClass('active');
+		}	
+		if ( ( scroll + navHeight ) >= ($('#work').offset().top) ){
+			$('li a').removeClass('active');
+			$('a[href="#work"]').addClass('active');
+		} else {
+			$('a[href="#work"]').removeClass('active');
+		}	
+		if($(window).scrollTop() + $(window).height()  == $(document).height()) {
+       		$('li a').removeClass('active');
+			$('a[href="#contact"]').addClass('active');
+		} else {
+			$('a[href="#contact"]').removeClass('active');
+		}
+	}
+
+});
