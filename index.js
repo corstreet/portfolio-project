753201f94f73f3bf7afbf1f31337f290024d0f3c
Corwin <corstreet@gmail.com>
2017-05-06 13:58:56 -0700

    adding react to the project

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Root = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default Root;
+render(<Root></Root>, document.getElementById("app"));
